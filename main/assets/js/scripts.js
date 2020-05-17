74191cafd4c3a55a6566a1e378a92fa4c97e2a54
Nico Calvo <calvonico@users.noreply.github.com>
2020-05-17 01:17:52 -0300

    Add files via upload

---
+++ main/assets/js/scripts.js
@@ -0,0 +1,533 @@
+// Utility function
+function Util () {};
+
+/* 
+	class manipulation functions
+*/
+Util.hasClass = function(el, className) {
+	if (el.classList) return el.classList.contains(className);
+	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+};
+
+Util.addClass = function(el, className) {
+	var classList = className.split(' ');
+ 	if (el.classList) el.classList.add(classList[0]);
+ 	else if (!Util.hasClass(el, classList[0])) el.className += " " + classList[0];
+ 	if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+	var classList = className.split(' ');
+	if (el.classList) el.classList.remove(classList[0]);	
+	else if(Util.hasClass(el, classList[0])) {
+		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+		el.className=el.className.replace(reg, ' ');
+	}
+	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+	if(bool) Util.addClass(el, className);
+	else Util.removeClass(el, className);
+};
+
+Util.setAttributes = function(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+/* 
+  DOM manipulation
+*/
+Util.getChildrenByClassName = function(el, className) {
+  var children = el.children,
+    childrenByClass = [];
+  for (var i = 0; i < el.children.length; i++) {
+    if (Util.hasClass(el.children[i], className)) childrenByClass.push(el.children[i]);
+  }
+  return childrenByClass;
+};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/* 
+	Animate height of an element
+*/
+Util.setHeight = function(start, to, element, duration, cb) {
+	var change = to - start,
+	    currentTime = null;
+
+  var animateHeight = function(timestamp){  
+    if (!currentTime) currentTime = timestamp;         
+    var progress = timestamp - currentTime;
+    var val = parseInt((progress/duration)*change + start);
+    element.style.height = val+"px";
+    if(progress < duration) {
+        window.requestAnimationFrame(animateHeight);
+    } else {
+    	cb();
+    }
+  };
+  
+  //set the height of the element before starting animation -> fix bug on Safari
+  element.style.height = start+"px";
+  window.requestAnimationFrame(animateHeight);
+};
+
+/* 
+	Smooth Scroll
+*/
+
+Util.scrollTo = function(final, duration, cb, scrollEl) {
+  var element = scrollEl || window;
+  var start = element.scrollTop || document.documentElement.scrollTop,
+    currentTime = null;
+
+  if(!scrollEl) start = window.scrollY || document.documentElement.scrollTop;
+      
+  var animateScroll = function(timestamp){
+  	if (!currentTime) currentTime = timestamp;        
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = Math.easeInOutQuad(progress, start, final-start, duration);
+    element.scrollTo(0, val);
+    if(progress < duration) {
+        window.requestAnimationFrame(animateScroll);
+    } else {
+      cb && cb();
+    }
+  };
+
+  window.requestAnimationFrame(animateScroll);
+};
+
+/* 
+  Focus utility classes
+*/
+
+//Move focus to an element
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName("body")[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+/* 
+  Misc
+*/
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+Util.cssSupports = function(property, value) {
+  if('CSS' in window) {
+    return CSS.supports(property, value);
+  } else {
+    var jsProperty = property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase();});
+    return jsProperty in document.body.style;
+  }
+};
+
+// merge a set of user options into plugin defaults
+// https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+Util.extend = function() {
+  // Variables
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  // Check if a deep merge
+  if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    deep = arguments[0];
+    i++;
+  }
+
+  // Merge the object into the extended object
+  var merge = function (obj) {
+    for ( var prop in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        // If deep merge and property is an object, merge properties
+        if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+          extended[prop] = extend( true, extended[prop], obj[prop] );
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for ( ; i < length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  if(!window.matchMedia) return false;
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+}; 
+
+/* 
+	Polyfills
+*/
+//Closest() method
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1); 
+		return null;
+	};
+}
+
+//Custom Event() constructor
+if ( typeof window.CustomEvent !== "function" ) {
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+}
+
+/* 
+	Animation curves
+*/
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+
+/* JS Utility Classes */
+(function() {
+  // make focus ring visible only for keyboard navigation (i.e., tab key) 
+  var focusTab = document.getElementsByClassName('js-tab-focus');
+  function detectClick() {
+    if(focusTab.length > 0) {
+      resetFocusTabs(false);
+      window.addEventListener('keydown', detectTab);
+    }
+    window.removeEventListener('mousedown', detectClick);
+  };
+
+  function detectTab(event) {
+    if(event.keyCode !== 9) return;
+    resetFocusTabs(true);
+    window.removeEventListener('keydown', detectTab);
+    window.addEventListener('mousedown', detectClick);
+  };
+
+  function resetFocusTabs(bool) {
+    var outlineStyle = bool ? '' : 'none';
+    for(var i = 0; i < focusTab.length; i++) {
+      focusTab[i].style.setProperty('outline', outlineStyle);
+    }
+  };
+  window.addEventListener('mousedown', detectClick);
+}());
+// File#: _1_anim-menu-btn
+// Usage: codyhouse.co/license
+(function() {
+  var menuBtns = document.getElementsByClassName('js-anim-menu-btn');
+  if( menuBtns.length > 0 ) {
+    for(var i = 0; i < menuBtns.length; i++) {(function(i){
+      initMenuBtn(menuBtns[i]);
+    })(i);}
+
+    function initMenuBtn(btn) {
+      btn.addEventListener('click', function(event){	
+        event.preventDefault();
+        var status = !Util.hasClass(btn, 'anim-menu-btn--state-b');
+        Util.toggleClass(btn, 'anim-menu-btn--state-b', status);
+        // emit custom event
+        var event = new CustomEvent('anim-menu-btn-clicked', {detail: status});
+        btn.dispatchEvent(event);
+      });
+    };
+  }
+}());
+// File#: _1_back-to-top
+// Usage: codyhouse.co/license
+(function() {
+  var backTop = document.getElementsByClassName('js-back-to-top')[0];
+  if( backTop ) {
+    var dataElement = backTop.getAttribute('data-element');
+    var scrollElement = dataElement ? document.querySelector(dataElement) : window;
+    var scrollDuration = parseInt(backTop.getAttribute('data-duration')) || 300, //scroll to top duration
+      scrollOffset = parseInt(backTop.getAttribute('data-offset')) || 0, //show back-to-top if scrolling > scrollOffset
+      scrolling = false;
+    
+    //detect click on back-to-top link
+    backTop.addEventListener('click', function(event) {
+      event.preventDefault();
+      if(!window.requestAnimationFrame) {
+        scrollElement.scrollTo(0, 0);
+      } else {
+        dataElement ? Util.scrollTo(0, scrollDuration, false, scrollElement) : Util.scrollTo(0, scrollDuration);
+      } 
+      //move the focus to the #top-element - don't break keyboard navigation
+      Util.moveFocus(document.getElementById(backTop.getAttribute('href').replace('#', '')));
+    });
+    
+    //listen to the window scroll and update back-to-top visibility
+    checkBackToTop();
+    if (scrollOffset > 0) {
+      scrollElement.addEventListener("scroll", function(event) {
+        if( !scrolling ) {
+          scrolling = true;
+          (!window.requestAnimationFrame) ? setTimeout(function(){checkBackToTop();}, 250) : window.requestAnimationFrame(checkBackToTop);
+        }
+      });
+    }
+
+    function checkBackToTop() {
+      var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+      if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+      Util.toggleClass(backTop, 'back-to-top--is-visible', windowTop >= scrollOffset);
+      scrolling = false;
+    }
+  }
+}());
+// File#: _1_smooth-scrolling
+// Usage: codyhouse.co/license
+(function() {
+  var SmoothScroll = function(element) {
+    this.element = element;
+    this.scrollDuration = parseInt(this.element.getAttribute('data-duration')) || 300;
+    this.dataElement = this.element.getAttribute('data-element');
+    this.scrollElement = this.dataElement ? document.querySelector(this.dataElement) : window;
+    this.initScroll();
+  };
+
+  SmoothScroll.prototype.initScroll = function() {
+    var self = this;
+
+    //detect click on link
+    this.element.addEventListener('click', function(event){
+      event.preventDefault();
+      var targetId = event.target.closest('.js-smooth-scroll').getAttribute('href').replace('#', ''),
+        target = document.getElementById(targetId),
+        targetTabIndex = target.getAttribute('tabindex'),
+        windowScrollTop = self.scrollElement.scrollTop || document.documentElement.scrollTop;
+
+      if(!self.dataElement) windowScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+      var scrollElement = self.dataElement ? self.scrollElement : false;
+
+      var fixedHeight = self.getFixedElementHeight(); // check if there's a fixed element on the page
+      Util.scrollTo(target.getBoundingClientRect().top + windowScrollTop - fixedHeight, self.scrollDuration, function() {
+        //move the focus to the target element - don't break keyboard navigation
+        Util.moveFocus(target);
+        history.pushState(false, false, '#'+targetId);
+        self.resetTarget(target, targetTabIndex);
+      }, scrollElement);
+    });
+  };
+
+  SmoothScroll.prototype.resetTarget = function(target, tabindex) {
+    if( parseInt(target.getAttribute('tabindex')) < 0) {
+      target.style.outline = 'none';
+      !tabindex && target.removeAttribute('tabindex');
+    }	
+  };
+
+  SmoothScroll.prototype.getFixedElementHeight = function() {
+    var fixedElementDelta = parseInt(getComputedStyle(document.documentElement).getPropertyValue('scroll-padding'));
+    if(isNaN(fixedElementDelta) ) { // scroll-padding not supported
+      fixedElementDelta = 0;
+      var fixedElement = document.querySelector(this.element.getAttribute('data-fixed-element'));
+      if(fixedElement) fixedElementDelta = parseInt(fixedElement.getBoundingClientRect().height);
+    }
+    return fixedElementDelta;
+  };
+  
+  //initialize the Smooth Scroll objects
+  var smoothScrollLinks = document.getElementsByClassName('js-smooth-scroll');
+  if( smoothScrollLinks.length > 0 && !Util.cssSupports('scroll-behavior', 'smooth') && window.requestAnimationFrame) {
+    // you need javascript only if css scroll-behavior is not supported
+    for( var i = 0; i < smoothScrollLinks.length; i++) {
+      (function(i){new SmoothScroll(smoothScrollLinks[i]);})(i);
+    }
+  }
+}());
+// File#: _1_vertical-timeline
+// Usage: codyhouse.co/license
+(function() {
+	var VTimeline = function(element) {
+    this.element = element;
+    this.sections = this.element.getElementsByClassName('js-v-timeline__section');
+    this.animate = this.element.getAttribute('data-animation') && this.element.getAttribute('data-animation') == 'on' ? true : false;
+    this.animationClass = 'v-timeline__section--animate';
+    this.animationDelta = '-150px';
+    initVTimeline(this);
+  };
+
+  function initVTimeline(element) {
+    if(!element.animate) return;
+    for(var i = 0; i < element.sections.length; i++) {
+      var observer = new IntersectionObserver(vTimelineCallback.bind(element, i),
+      {rootMargin: "0px 0px "+element.animationDelta+" 0px"});
+		  observer.observe(element.sections[i]);
+    }
+  };
+
+  function vTimelineCallback(index, entries, observer) {
+    if(entries[0].isIntersecting) {
+      Util.addClass(this.sections[index], this.animationClass);
+      observer.unobserve(this.sections[index]);
+    } 
+  };
+
+  //initialize the VTimeline objects
+  var timelines = document.querySelectorAll('.js-v-timeline'),
+    intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype),
+    reducedMotion = Util.osHasReducedMotion();
+	if( timelines.length > 0) {
+		for( var i = 0; i < timelines.length; i++) {
+      if(intersectionObserverSupported && !reducedMotion) (function(i){new VTimeline(timelines[i]);})(i);
+      else timelines[i].removeAttribute('data-animation');
+		}
+	}
+}());
+// File#: _2_flexi-header
+// Usage: codyhouse.co/license
+(function() {
+  var flexHeader = document.getElementsByClassName('js-f-header');
+  if(flexHeader.length > 0) {
+    var menuTrigger = flexHeader[0].getElementsByClassName('js-anim-menu-btn')[0],
+      firstFocusableElement = getMenuFirstFocusable();
+
+    // we'll use these to store the node that needs to receive focus when the mobile menu is closed 
+    var focusMenu = false;
+
+    menuTrigger.addEventListener('anim-menu-btn-clicked', function(event){ // toggle menu visibility an small devices
+      Util.toggleClass(document.getElementsByClassName('f-header__nav')[0], 'f-header__nav--is-visible', event.detail);
+      menuTrigger.setAttribute('aria-expanded', event.detail);
+      if(event.detail) firstFocusableElement.focus(); // move focus to first focusable element
+      else if(focusMenu) {
+        focusMenu.focus();
+        focusMenu = false;
+      }
+    });
+
+    // listen for key events
+    window.addEventListener('keyup', function(event){
+      // listen for esc key
+      if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+        // close navigation on mobile if open
+        if(menuTrigger.getAttribute('aria-expanded') == 'true' && isVisible(menuTrigger)) {
+          focusMenu = menuTrigger; // move focus to menu trigger when menu is close
+          menuTrigger.click();
+        }
+      }
+      // listen for tab key
+      if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+        // close navigation on mobile if open when nav loses focus
+        if(menuTrigger.getAttribute('aria-expanded') == 'true' && isVisible(menuTrigger) && !document.activeElement.closest('.js-f-header')) menuTrigger.click();
+      }
+    });
+
+    function getMenuFirstFocusable() {
+      var focusableEle = flexHeader[0].getElementsByClassName('f-header__nav')[0].querySelectorAll('[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex]:not([tabindex="-1"]), [contenteditable], audio[controls], video[controls], summary'),
+        firstFocusable = false;
+      for(var i = 0; i < focusableEle.length; i++) {
+        if( focusableEle[i].offsetWidth || focusableEle[i].offsetHeight || focusableEle[i].getClientRects().length ) {
+          firstFocusable = focusableEle[i];
+          break;
+        }
+      }
+
+      return firstFocusable;
+    };
+    
+    function isVisible(element) {
+      return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+    };
+  }
+}());
+// File#: _2_sticky-sharebar
+// Usage: codyhouse.co/license
+(function() {
+  var StickyShareBar = function(element) {
+    this.element = element;
+    this.contentTarget = document.getElementsByClassName('js-sticky-sharebar-target');
+    this.showClass = 'sticky-sharebar--on-target';
+    this.threshold = '50%'; // Share Bar will be revealed when .js-sticky-sharebar-target element reaches 50% of the viewport
+    initShareBar(this);
+  };
+
+  function initShareBar(shareBar) {
+    if(shareBar.contentTarget.length < 1) {
+      Util.addClass(shareBar.element, shareBar.showClass);
+      return;
+    }
+    if(intersectionObserverSupported) {
+      initObserver(shareBar); // update anchor appearance on scroll
+    } else {
+      Util.addClass(shareBar.element, shareBar.showClass);
+    }
+  };
+
+  function initObserver(shareBar) {
+    var observer = new IntersectionObserver(
+      function(entries, observer) { 
+        Util.toggleClass(shareBar.element, shareBar.showClass, entries[0].isIntersecting);
+      }, 
+      {rootMargin: "0px 0px -"+shareBar.threshold+" 0px"}
+    );
+    observer.observe(shareBar.contentTarget[0]);
+  };
+
+  //initialize the StickyShareBar objects
+  var stickyShareBar = document.getElementsByClassName('js-sticky-sharebar'),
+    intersectionObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype);
+  
+  if( stickyShareBar.length > 0 ) {
+    for( var i = 0; i < stickyShareBar.length; i++) {
+      (function(i){ new StickyShareBar(stickyShareBar[i]); })(i);
+    }
+  }
+}());
